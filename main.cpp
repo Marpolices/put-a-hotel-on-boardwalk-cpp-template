8d7a9fb64a21abdb04ea02bda3b80b83ce6a4d4b
Marpolices <144827972+Marpolices@users.noreply.github.com>
2025-09-18 14:49:06 +0000

    submitting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,89 @@
 #include <iostream>
+#include <string>
 
-#include "src/factorial.hpp"
+using namespace std; 
+
+template <typename T>
+class CircularLinkedList {
+private:
+  struck Node {
+    T data;
+    Node* next;
+    Node(const T& value) : data(value), next(nullptr) {}
+  };
+
+  Node* head;
+  Node* tail;
+
+  public:
+  Node* current;
+
+  CircularLinkedList() : head(nullptr)
+}
+
+
+
+
+
+
 
 int main() {
-  std::cout << "The factorial of 5 is " << factorial(5) << std::endl;
+    CircularLinkedList<string> monopolyBoard;
+
+    
+    monopolyBoard.append("Go");
+    monopolyBoard.append("Mediterranean Avenue");
+    monopolyBoard.append("Community Chest");
+    monopolyBoard.append("Baltic Avenue");
+    monopolyBoard.append("Income Tax");
+    monopolyBoard.append("Reading Railroad");
+    monopolyBoard.append("Oriental Avenue");
+    monopolyBoard.append("Chance");
+    monopolyBoard.append("Vermont Avenue");
+    monopolyBoard.append("Connecticut Avenue");
+    monopolyBoard.append("Jail (Just Visiting)");
+    monopolyBoard.append("St. Charles Place");
+    monopolyBoard.append("Electric Company");
+    monopolyBoard.append("States Avenue");
+    monopolyBoard.append("Virginia Avenue");
+    monopolyBoard.append("Pennsylvania Railroad");
+    monopolyBoard.append("St. James Place");
+    monopolyBoard.append("Community Chest");
+    monopolyBoard.append("Tennessee Avenue");
+    monopolyBoard.append("New York Avenue");
+    monopolyBoard.append("Free Parking");
+    monopolyBoard.append("Kentucky Avenue");
+    monopolyBoard.append("Chance");
+    monopolyBoard.append("Indiana Avenue");
+    monopolyBoard.append("Illinois Avenue");
+    monopolyBoard.append("B&O Railroad");
+    monopolyBoard.append("Atlantic Avenue");
+    monopolyBoard.append("Ventnor Avenue");
+    monopolyBoard.append("Water Works");
+    monopolyBoard.append("Marvin Gardens");
+    monopolyBoard.append("Go to Jail");
+    monopolyBoard.append("Pacific Avenue");
+    monopolyBoard.append("North Carolina Avenue");
+    monopolyBoard.append("Community Chest");
+    monopolyBoard.append("Pennsylvania Avenue");
+    monopolyBoard.append("Short Line Railroad");
+    monopolyBoard.append("Chance");
+    monopolyBoard.append("Park Place");
+    monopolyBoard.append("Luxury Tax");
+    monopolyBoard.append("Boardwalk");
+
+  cout << "Starting position: " << monopolyBoard.currentNode() << "\n";
+
+  monopolyBoard.step();
+  cout << "After 1 step: " << monopolyBoard.currentNode() << "\n";
+
+  monopolyBoard.step();
+  monopolyBoard.step();
+  monopolyBoard.step();
+  cout << "After 4 step: " << monopolyBoard.currentNode() << "\n";
+
+  for (int i = 0; i < 37; i++) {
+    monopoly.step();
+  }
+  cout << "After looping around: " << monopolyBoard.currentNode() << "\n";
 }
